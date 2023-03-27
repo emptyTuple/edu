671051befc0748c0cb7ad28f0021cc5b66602f6d
emptyTuple <kotorov@outlook.com>
2023-03-28 00:47:02 +0700

    first commit

--- src/edu/zip/AddFolderToZipRecursion.java
+++ src/edu/zip/AddFolderToZipRecursion.java
@@ -1,15 +1,32 @@
 package edu.zip;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.zip.ZipOutputStream;
 
 public class AddFolderToZipRecursion {
 
     public static void main(String[] args) {
-        addFolderToZipRecursion();
+        String folder = "/Users/emptytuple/sandbox/archive_root";
+        File[] rootFolderFiles = new File(folder).listFiles();
+
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(folder))) {
+            addFolderToZipRecursion(zout, rootFolderFiles, "");
+
+        } catch (IOException e) {
+            e.getCause();
+        }
     }
 
-    public static void addFolderToZipRecursion() {
-
+    public static void addFolderToZipRecursion(ZipOutputStream zout, File[] rootFiles, String base) {
+        for (File file : rootFiles) {
+            if (file.isFile()) {
+                System.out.println(file.toString());
+//                addFolderToZipRecursion(zout, file.listFiles(), base + file.getName() + File.pathSeparator);
+            }
+        }
     }
 }

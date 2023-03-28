2e785a7bca92c4e25ee1c406ed4eeb39ea1bf5b1
emptyTuple <kotorov@outlook.com>
2023-03-28 21:41:02 +0700

    28/03/2023

---
+++ src/edu/algorithms/BubbleSort.java
@@ -0,0 +1,40 @@
+package edu.algorithms;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+O(n^2)
+Сортировка простыми обменами, сортировка пузырьком.
+Можно ускорить если обходить массив в прямом и обратном порядке за один проход.
+Можно ускорить если не проходить по отсортированному краю.
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        Random r = new Random(10);
+        for(int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(99);
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("________sorting________");
+        bubbleSort(array);
+    }
+
+    private static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                    isSorted = false;
+                }
+            }
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
